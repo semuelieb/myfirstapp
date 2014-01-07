d83ffa2769c16ae465123c6f96da0f0f522a48c9
semlie <israellieb@gmail.com>
2014-01-08 01:56:31 +0200

    I insert plush in session

--- mailapp.py
+++ mailapp.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 import views
+from flask.ext.pymongo import PyMongo
+
 
 app = Flask(__name__)
 app.secret_key = 'aaa'
-users = [{'user': 'sem', 'passwd': 1234}]
+mongo = PyMongo(app)
+users = {'user': 'sem', 'passwd': 1234}
 
 
 @app.route('/s/<name>')
@@ -12,20 +15,24 @@ def hello_world(name):
 @app.route('/')
 def helo():
 	name ='my name'
+	for x in xrange(1,10):
+		flash("helo world")
 	return render_template('index.html',name = name)
 
 @app.route('/register', methods=['GET', 'POST'])
 def submit():
     form = views.MyForm()
     if form.validate_on_submit():
-        return redirect('/success')
+    	if request.method=='POST':
+    		if users['user']==request.form['name'] :
+    			return request.form['name']+request.form['passwd']
+	flash('Err '+request.form['name']+request.form['passwd']+'incurrect')
     return render_template('register.html', form=form)
 @app.route('/success', methods=['GET', 'POST'])
 def succes():
 	if request.method=='POST':
 		return request.form['name'],request.form['passwd']
-	print request.form['name']
-	return submit()
+	return render_template('register.html')
 
 if __name__ == '__main__':
 	app.debug = True
